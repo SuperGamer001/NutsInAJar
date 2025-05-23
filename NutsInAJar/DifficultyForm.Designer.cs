9f6b43b85f7f5c8d9dafcce3e71ccbd1d57db1eb
Alex Fischer <183024988+SuperGamer001@users.noreply.github.com>
2025-05-22 20:06:26 -0700

    Add project files.

---
+++ NutsInAJar/DifficultyForm.Designer.cs
@@ -0,0 +1,95 @@
+﻿namespace NutsInAJar
+{
+    partial class DifficultyForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            label1 = new Label();
+            button1 = new Button();
+            button2 = new Button();
+            button3 = new Button();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(12, 9);
+            label1.Name = "label1";
+            label1.Size = new Size(258, 15);
+            label1.TabIndex = 0;
+            label1.Text = "Before the game begins, please pick a difficulty.\r\n";
+            // 
+            // button1
+            // 
+            button1.Location = new Point(80, 76);
+            button1.Name = "button1";
+            button1.Size = new Size(113, 23);
+            button1.TabIndex = 1;
+            button1.Text = "Easy (0 - 10)";
+            button1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            button2.Location = new Point(80, 141);
+            button2.Name = "button2";
+            button2.Size = new Size(113, 23);
+            button2.TabIndex = 2;
+            button2.Text = "Normal (0 - 30)";
+            button2.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            button3.Location = new Point(80, 207);
+            button3.Name = "button3";
+            button3.Size = new Size(113, 23);
+            button3.TabIndex = 3;
+            button3.Text = "Hard (0 - 80)";
+            button3.UseVisualStyleBackColor = true;
+            // 
+            // DifficultyForm
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(285, 306);
+            Controls.Add(button3);
+            Controls.Add(button2);
+            Controls.Add(button1);
+            Controls.Add(label1);
+            Name = "DifficultyForm";
+            Text = "DifficultyForm";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label label1;
+        private Button button1;
+        private Button button2;
+        private Button button3;
+    }
+}
---
+++ NutsInAJar/Form1.Designer.cs
@@ -0,0 +1,96 @@
+﻿namespace NutsInAJar
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            label1 = new Label();
+            IGuessBTN = new Button();
+            label2 = new Label();
+            AIGuessBTN = new Button();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(60, 9);
+            label1.Name = "label1";
+            label1.Size = new Size(206, 15);
+            label1.TabIndex = 0;
+            label1.Text = "Welcome to the NUTS IN A JAR Game";
+            // 
+            // IGuessBTN
+            // 
+            IGuessBTN.Location = new Point(90, 134);
+            IGuessBTN.Name = "IGuessBTN";
+            IGuessBTN.Size = new Size(146, 61);
+            IGuessBTN.TabIndex = 1;
+            IGuessBTN.Text = "I will guess";
+            IGuessBTN.UseVisualStyleBackColor = true;
+            IGuessBTN.Click += IGuessBTN_Click;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(12, 44);
+            label2.Name = "label2";
+            label2.Size = new Size(312, 30);
+            label2.TabIndex = 2;
+            label2.Text = "In this game, either you or the AI has to guess the number\r\nof nuts in a jar the other provides. Good luck!\r\n";
+            // 
+            // AIGuessBTN
+            // 
+            AIGuessBTN.Location = new Point(90, 221);
+            AIGuessBTN.Name = "AIGuessBTN";
+            AIGuessBTN.Size = new Size(146, 61);
+            AIGuessBTN.TabIndex = 3;
+            AIGuessBTN.Text = "The AI will guess";
+            AIGuessBTN.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(332, 366);
+            Controls.Add(AIGuessBTN);
+            Controls.Add(label2);
+            Controls.Add(IGuessBTN);
+            Controls.Add(label1);
+            Name = "Form1";
+            Text = "Form1";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label label1;
+        private Button IGuessBTN;
+        private Label label2;
+        private Button AIGuessBTN;
+    }
+}
